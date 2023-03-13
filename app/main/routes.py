76c27b558adfdc3f91f911b7bd39ef0af331032f
nick3053 <nick97+git@protonmail.com>
2023-03-13 15:42:47 +0100

    Commit before DB migrate

---
+++ app/main/routes.py
@@ -0,0 +1,255 @@
+from datetime import datetime
+from io import BytesIO
+from flask import render_template, flash, redirect, url_for, make_response, send_file, Response, abort, request, jsonify, current_app
+from app import db
+from app.main.forms import CSRForm, CertForm, EditProfileForm, ConvertCertificateForm
+from flask_login import current_user, login_required
+from app.models import User, Certificate
+from OpenSSL import crypto
+from app.main import bp
+from OpenSSL import SSL
+import base64
+
+
+@bp.route('/')
+@bp.route('/index')
+@login_required
+def index():
+    user = {'username': 'test'}
+
+    return render_template("index.html", title='Home Page')
+
+@bp.route("/generate_csr", methods=["GET", "POST"])
+@login_required
+def generate_csr():
+    form = CSRForm()
+    if form.validate_on_submit():
+        # Set default values for optional fields
+        organizational_unit = ""
+        # retrieve form data and generate CSR
+        data = request.form
+        country = data['country']
+        state = data['state']
+        locality = data['locality']
+        organization = data['organization']
+        organizational_unit = data['organizational_unit'] or ""
+        common_name = data['common_name']
+        subject_alternative_name = data['subject_alternative_name']
+
+        # Check if CSR with common name already exists
+        existing_csr = Certificate.query.filter_by(cn=common_name, user_id=current_user.id).first()
+        if existing_csr:
+            flash(f"A certificate with common name '{common_name}' already exists.")
+            return render_template("generate_csr.html", title='Generate CSR', form=form, data=data)
+
+        #Create Keypair
+        keypair = crypto.PKey()
+        keypair.generate_key(crypto.TYPE_RSA, 2048)
+
+        # Create a certificate request
+        req = crypto.X509Req()
+        req.get_subject().CN = common_name
+        req.get_subject().C = country
+        req.get_subject().ST = state
+        req.get_subject().L = locality
+        req.get_subject().O = organization
+        if organizational_unit:
+            req.get_subject().OU = organizational_unit
+        x509_extensions = ([])
+        sans_list = []
+        for san in subject_alternative_name:
+            sans_list.append("DNS: {0}".format(san))
+
+        sans_list = ", ".join(sans_list).encode()
+
+        if sans_list:
+            x509_extensions.append(crypto.X509Extension("subjectAltName".encode(), False, sans_list))
+
+        req.add_extensions(x509_extensions)
+        req.set_pubkey(keypair)
+        req.sign(keypair, "sha256")
+
+        key = crypto.dump_privatekey(crypto.FILETYPE_PEM, keypair)
+        csr = crypto.dump_certificate_request(crypto.FILETYPE_PEM, req)
+
+        certificate = Certificate(csr=csr,author=current_user,cn=common_name,organization=organization,key=key)
+        db.session.add(certificate)
+        db.session.commit()
+        
+        #response = b'\n'.join([csr, key])
+        return render_template('csr.html', cn=common_name,csr=csr, key=key)
+
+
+    return render_template("generate_csr.html", title='Generate CSR', form=form)
+
+@bp.route("/download_certificate", methods=["GET", "POST"])
+@login_required
+def download_certificate():
+    form = CertForm()
+    if form.validate_on_submit():
+        try:
+            # Load the existing key from the database
+            existing_key = Certificate.query.filter_by(cn=form.common_name.data, user_id=current_user.id).first()
+            private_key = existing_key.key
+            if not existing_key:
+                flash('The Private Key for the Common Name in the CSR does not exist in the database')
+                return render_template('download_error.html', title='Error')
+
+            # decode the private and public keys from base64 and add padding if needed
+            certificate = (form.certificate.data)
+            passphrase = (form.password.data)
+            
+            # Load private key
+            pkey = crypto.load_privatekey(crypto.FILETYPE_PEM, private_key)
+
+            # Load public key (certificate)
+            cert = crypto.load_certificate(crypto.FILETYPE_PEM, certificate)
+            
+            # Create PKCS#12 (PFX) file
+            pfx = crypto.PKCS12()
+            pfx.set_privatekey(pkey)
+            pfx.set_certificate(cert)
+            pfx_data = pfx.export(passphrase=passphrase)
+
+            # Save the PFX to the database
+            existing_key.pfx = pfx_data
+            db.session.commit()
+
+            # Return the PFX as a download
+            filename = existing_key.cn + '.pfx'
+            response = make_response(pfx_data)
+            response.headers['Content-Disposition'] = 'attachment; filename={}'.format(filename)
+            response.headers.set('Content-Type', 'application/x-pkcs12')
+            return response
+        
+        except Exception as e:
+            error_message = str(e)
+            return render_template('convert_error.html', title='Error Converting Certificate', error_message=error_message)
+               
+    return render_template('download_certificate.html', title='Download Certificate', form=form)
+
+@bp.route('/convert_certificate', methods=['GET', 'POST'])
+def convert_certificate():
+    form = ConvertCertificateForm()
+    if form.validate_on_submit():
+        try:
+            # decode the private and public keys from base64 and add padding if needed
+            private_key = (form.private_key.data)
+            public_key = (form.public_key.data)
+            passphrase = (form.password.data)
+            
+            # Load private key
+            pkey = crypto.load_privatekey(crypto.FILETYPE_PEM, private_key)
+
+            # Load public key (certificate)
+            cert = crypto.load_certificate(crypto.FILETYPE_PEM, public_key)
+            
+            # Create PKCS#12 (PFX) file
+            pfx = crypto.PKCS12()
+            pfx.set_privatekey(pkey)
+            pfx.set_certificate(cert)
+            pfx_data = pfx.export(passphrase=passphrase)
+
+            # Return the PFX as a download
+            response = make_response(pfx_data)
+            response.headers.set('Content-Disposition', 'attachment', filename='certificate.pfx')
+            response.headers.set('Content-Type', 'application/x-pkcs12')
+            return response
+        
+        except Exception as e:
+            error_message = str(e)
+            return render_template('convert_error.html', title='Error Converting Certificate', error_message=error_message)
+        
+    return render_template('convert_certificate.html', title='Convert Certificate', form=form)    
+
+@bp.route('/csr/download/<string:cn>')
+@login_required
+def download_csr(cn):
+    csr = Certificate.query.filter_by(cn=cn, user_id=current_user.id).first()
+    if csr is None:
+        abort(404)
+    file_data = csr.csr
+    # Create an in-memory file-like object
+    file_stream = BytesIO(file_data)
+    # Set the file stream's position to the beginning
+    file_stream.seek(0)
+    # Return the file as an attachment
+    filename = csr.cn + '.csr'
+    response = make_response(file_stream.getvalue())
+    response.headers['Content-Disposition'] = 'attachment; filename={}'.format(filename)
+    response.mimetype = 'text/plain'
+    return response
+
+@bp.route('/key/download/<string:cn>')
+@login_required
+def download_key(cn):
+    key = Certificate.query.filter_by(cn=cn, user_id=current_user.id).first()
+    if key is None:
+        abort(404)
+    file_data = key.key
+    # Create an in-memory file-like object
+    file_stream = BytesIO(file_data)
+    # Set the file stream's position to the beginning
+    file_stream.seek(0)
+    # Return the file as an attachment
+    filename = key.cn + '.key'
+    response = make_response(file_stream.getvalue())
+    response.headers['Content-Disposition'] = 'attachment; filename={}'.format(filename)
+    response.mimetype = 'text/plain'
+    return response
+
+@bp.route('/pfx/download/<string:cn>')
+@login_required
+def download_pfx(cn):
+    certificate = Certificate.query.filter_by(cn=cn, user_id=current_user.id).first()
+    if certificate is None:
+        abort(404)
+    pfx_data = certificate.pfx
+    filename = certificate.cn + '.pfx'
+    response = make_response(pfx_data)
+    response.headers['Content-Disposition'] = 'attachment; filename={}'.format(filename)
+    response.headers.set('Content-Type', 'application/x-pkcs12')
+    return response
+
+@bp.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    ##certificates = user.certificates.order_by(Certificate.timestamp.desc())
+    page = request.args.get('page', 1, type=int)
+    certificates = current_user.certificates.order_by(Certificate.timestamp.desc()).paginate(
+        page=page, per_page=current_app.config['CERTIFICATES_PER_PAGE'], error_out=False)
+    next_url = url_for('main.user', username=user.username, page=certificates.next_num) \
+        if certificates.has_next else None
+    prev_url = url_for('main.user', username=user.username, page=certificates.prev_num) \
+        if certificates.has_prev else None
+    return render_template('user.html', title='User Table', user=user, certificates=certificates,
+                           next_url=next_url, prev_url=prev_url)
+
+@bp.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm(current_user.username, current_user.email)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('main.edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
+
+
+@bp.route('/delete_certificate/<int:id>', methods=['POST'])
+@login_required
+def delete_certificate(id):
+    certificate = Certificate.query.get_or_404(id)
+    if certificate.user_id != current_user.id:
+        abort(403)
+    db.session.delete(certificate)
+    db.session.commit()
+    flash('Certificate deleted.')
+    return redirect(url_for('main.user', username=current_user.username))
--- app/models.py
+++ app/models.py
@@ -3,8 +3,33 @@ from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
+from flask import current_app, url_for
 
-class User(UserMixin, db.Model):
+class PaginatedAPIMixin(object):
+    @staticmethod
+    def to_collection_dict(query, page, per_page, endpoint, **kwargs):
+        resources = query.paginate(page=page, per_page=per_page,
+                                   error_out=False)
+        data = {
+            'items': [item.to_dict() for item in resources.items],
+            '_meta': {
+                'page': page,
+                'per_page': per_page,
+                'total_pages': resources.pages,
+                'total_items': resources.total
+            },
+            '_links': {
+                'self': url_for(endpoint, page=page, per_page=per_page,
+                                **kwargs),
+                'next': url_for(endpoint, page=page + 1, per_page=per_page,
+                                **kwargs) if resources.has_next else None,
+                'prev': url_for(endpoint, page=page - 1, per_page=per_page,
+                                **kwargs) if resources.has_prev else None
+            }
+        }
+        return data
+
+class User(UserMixin, PaginatedAPIMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -19,6 +44,25 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def to_dict(self):
+        data = {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'certificates_count' : self.certificates.count(),
+            '_links': {
+                'self': url_for('api.get_user', id=self.id)
+            }
+            }
+        return data
+    
+    def from_dict(self, data, new_user=False):
+        for field in ['username', 'email']:
+            if field in data:
+                setattr(self, field, data[field])
+        if new_user and 'password' in data:
+            self.set_password(data['password'])
 
 @login.user_loader
 def load_user(id):
@@ -30,9 +74,25 @@ class Certificate(db.Model):
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     csr = db.Column(db.LargeBinary)
     key = db.Column(db.LargeBinary)
-    cn = db.Column(db.String(64))
-    organization = db.Column(db.String(64))
+    cn = db.Column(db.String(64), index=True)
+    organization = db.Column(db.String(64), index=True)
     pfx = db.Column(db.LargeBinary)
-    
+
+    def to_dict(self):
+        data = {
+            'id': self.id,
+            'csr': self.csr,
+            'key': self.key,
+            'cn': self.cn,
+            'pfx': self.pfx,
+            '_links': {
+                'csr': url_for('api.get_csr', cn=self.cn),
+                'key': url_for('api.get_key', cn=self.cn),
+                'pfx': url_for('api.get_pfx', cn=self.cn),
+            }
+            }
+        return data
+
+
     def __repr__(self):
         return '<Certificate {}>}'.format(self.csr)
