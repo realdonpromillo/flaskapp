21b3796fc21b86b463367cfc15db5a272beec478
realdonpromillo <61872897+realdonpromillo@users.noreply.github.com>
2023-03-19 09:53:56 +0100

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 source venv/bin/activate
-flask db upgrade
-exec gunicorn -b :5000 --access-logfile - --error-logfile - cert:app
+while true; do
+    flask db upgrade
+    if [[ "$?" == "0" ]]; then
+        break
+    fi
+    echo Deploy command failed, retrying in 5 secs...
+    sleep 5
+done
+exec gunicorn -b :5000 --access-logfile - --error-logfile - certificates:app
